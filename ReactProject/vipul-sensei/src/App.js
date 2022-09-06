57e16c1c3fcd265b6d2f1c5b6f816aeec1ecd86f
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2022-09-06 11:02:56 +0530

    6Sep2022 - Some styling in react

--- ReactProject/vipul-sensei/src/App.js
+++ ReactProject/vipul-sensei/src/App.js
@@ -1,32 +1,20 @@
-// import {
-//   Routes,
-//   Route,
-//   Link,
-//   useNavigate
-// } from "react-router-dom";
-// import {useEffect, useState} from "react";
-// import Login from "./components/Login";
-// import Registration from "./components/Registration";
-import { useState } from "react";
-import Home from "./components/Home";
-// import Welcome from "./components/Welcome";
-import Background from "./static/backgrounds/jeremy-bishop-G9i_plbfDgk-unsplash.jpg";
+// import { useState } from "react";
+// import Home from "./components/Home";
+// import Background from "./static/backgrounds/jeremy-bishop-G9i_plbfDgk-unsplash.jpg";
 
 function App() {
-  const [num1, setNum1] = useState(0);
-  const [num2, setNum2] = useState(0);
-  const [string, setString] = useState("");
+  // const [num1, setNum1] = useState(0);
+  // const [num2, setNum2] = useState(0);
+  // const [string, setString] = useState("");
+  // style={{ backgroundImage: `url(${Background})` }}
   return (
     <div
-      className="App flex flex-col min-h-screen w-full bg-pc-8 text-white bg-no-repeat bg-center bg-cover"
-      style={{ backgroundImage: `url(${Background})` }}
+      className="App flex flex-col min-h-screen w-full bg-white text-white bg-no-repeat bg-center bg-cover px-8"
     >
-      <div className="flex flex-col py-8 w-64 sm:w-96 mx-auto">
-        <input className="py-1 bg-black/50 px-2 text-lg border border-cyan-400 outline-none m-3" value={num1} type="number" onChange={e => setNum1(e.currentTarget.value)} placeholder="Even or Odd" />
-        <input className="py-1 bg-black/50 px-2 text-lg border border-cyan-400 outline-none m-3" value={num2} type="number" onChange={e => setNum2(e.currentTarget.value)} placeholder="Prime or not" />
-        <input className="py-1 bg-black/50 px-2 text-lg border border-cyan-400 outline-none m-3" value={string} onChange={e => setString(e.currentTarget.value)} placeholder="Palindrome or not" />
+      <div className="my-12 mx-auto rounded w-full pt-6 px-6 pb-36 border-2 border-neutral-900 text-neutral-900 max-w-xl bg-neutral-100">
+        <div className="text-4xl mb-8">Hello Task Tracker</div>
+        <button className="py-2 px-6 bg-neutral-900 text-white rounded text-lg">Add Task</button>
       </div>
-      <Home num1={Number(num1)} num2={Number(num2)} string={string} />
     </div>
   );
 }
--- ReactProject/vipul-sensei/src/components/Welcome.jsx
+++
@@ -1,5 +0,0 @@
-export default function Welcome({User}) {
-  return (
-    <div className="flex-1 px-8 pb-20 flex flex-col justify-center items-center text-7xl text-cyan-400 ">Welcome {(User && User.Name) || "Hacker!"}</div>
-  );
-}
