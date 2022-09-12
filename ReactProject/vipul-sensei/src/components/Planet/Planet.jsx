a3152fed6906b9ca6934e96a8aee4290e611334f
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2022-09-12 20:45:36 +0530

    updated responsiveness

--- ReactProject/vipul-sensei/src/components/Planet/Planet.jsx
+++ ReactProject/vipul-sensei/src/components/Planet/Planet.jsx
@@ -1,8 +1,8 @@
-export default function Planet({image, size, ratio, name, onClick}) {
+export default function Planet({image, ratio, name, onClick}) {
     return (
         <button
             onClick={() => onClick(name, ratio)}
-            className={`h-32 w-44 m-8 bg-no-repeat bg-cover bg-center`}
+            className={`h-24 sm:h-32 lg:h-44 w-36 sm:w-44 lg:w-64 m-8 bg-no-repeat bg-cover bg-center`}
             style={{backgroundImage: `url(${image})`}}
         ></button>
     );
