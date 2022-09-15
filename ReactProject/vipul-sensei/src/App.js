fe45e3a04a5cda46409fdeaa7a38a7959816343f
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2022-09-15 14:35:06 +0530

    You should avoid looking in this commit!

--- ReactProject/vipul-sensei/src/App.js
+++ ReactProject/vipul-sensei/src/App.js
@@ -1,24 +1,21 @@
-import Background from "./static/backgrounds/elliott-engelmann-DjlKxYFJlTc-unsplash.jpg";
+import Background from "./static/backgrounds/emma-birman-on8ih2Em_vA-unsplash.jpg";
 
-import { Routes, Route } from "react-router-dom";
+/*import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import About from "./components/About/About";
+import About from "./components/About/About";*/
+
+import AppStyles from './App.module.css';
 
 function App() {
   return (
     <div
       style={{backgroundImage: `url(${Background})`}}
-      className="App min-h-screen w-full text-white bg-no-repeat bg-center bg-cover overflow-x-hidden"
+      className="App min-h-screen w-full text-white bg-black bg-no-repeat bg-center bg-cover overflow-x-hidden"
     >
-      <div style={{backdropFilter: `blur(5px)`}} className="flex flex-col items-center justify-between min-h-screen w-full">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
+      <div style={{backdropFilter: `blur(5px)`}} className="flex flex-col items-center justify-between min-h-screen w-full p-16">
+        <h1 className={AppStyles.text}>This is a component of our website!</h1>
       </div>
     </div>
   );
